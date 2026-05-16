6f648c0c291a72348a1ef0fc42a1846136ad7fcc
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-05-16 15:17:25 +0530

    Time: 3 ms (89.02%), Space: 46.6 MB (60.24%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    void fn(int ind, int[]nums, boolean[]flag,List<Integer>ds, List<List<Integer>>ans){
+        if(ds.size()==nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+          if(i > 0 && nums[i] == nums[i-1] && !flag[i-1]) continue;
+            if(!flag[i]){
+                ds.add(nums[i]);
+                flag[i]=true;
+                fn(i+1,nums,flag,ds,ans);
+                ds.remove(ds.size()-1);
+                flag[i]=false;
+            }
+        }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        boolean [] flag =new boolean[nums.length];
+        List<List<Integer>> ans=new ArrayList<>();
+        fn(0,nums,flag,new ArrayList<>(),ans);
+        return ans;
+    }
+}
