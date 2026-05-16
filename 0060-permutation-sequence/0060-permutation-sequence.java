5e303fd7f8c9417958461683883f564b39f984eb
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-05-16 15:32:08 +0530

    Time: 1606 ms (5%), Space: 282.5 MB (5%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -0,0 +1,35 @@
+class Solution {
+    void fn(int ind, int[] nums, boolean[] flag, List<Integer> ds, List<List<Integer>> ans) {
+        if (ds.size() == nums.length) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!flag[i]) {
+                ds.add(nums[i]);
+                flag[i] = true;
+                fn(i + 1, nums, flag, ds, ans);
+                ds.remove(ds.size() - 1);
+                flag[i] = false;
+            }
+        }
+    }
+
+    public String getPermutation(int n, int k) {
+        boolean[] flag = new boolean[n];
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        fn(0, nums, flag, new ArrayList<>(), ans);
+        List<Integer> res = ans.get(k - 1);
+        StringBuilder sb = new StringBuilder();
+
+        for (int num : res) {
+            sb.append(num);
+        }
+
+        return sb.toString();
+    }
+}
