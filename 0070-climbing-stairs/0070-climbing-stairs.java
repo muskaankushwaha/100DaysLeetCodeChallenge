9ef71cf4a8ad37c3444f704936f63fd9151836de
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-05-29 19:44:41 +0530

    Time: 0 ms (100%), Space: 42.2 MB (16.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,15 +8,25 @@ class Solution {
         // dp[n]=climbStairs(n-1)+climbStairs(n-2);
         // return dp[n];
 
-        //Tabulization
-        int [] dp=new int [n+1];
-        Arrays.fill(dp,-1);
-        dp[0]=1;
-        dp[1]=1;
-        if(dp[n]!=-1)return dp[n];
+        // //Tabulization
+        // int [] dp=new int [n+1];
+        // Arrays.fill(dp,-1);
+        // dp[0]=1;
+        // dp[1]=1;
+        // if(dp[n]!=-1)return dp[n];
+        // for(int i=2; i<=n; i++){
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
+
+        //Space optimization
+        int prev2=1;
+        int prev=1;
         for(int i=2; i<=n; i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int curr=prev2+prev;
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n];
+        return prev;
     }
 }
