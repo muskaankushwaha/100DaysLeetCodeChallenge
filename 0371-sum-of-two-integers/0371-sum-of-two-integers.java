348c12076a78e2ec8b661a4bad399bf479405862
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-06-16 19:07:28 +0530

    Time: 0 ms (100%), Space: 42.1 MB (31.46%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,5 +1,7 @@
 class Solution {
     public int getSum(int a, int b) {
+        //xor for sum
+        //& for carry and << to shift carry
         while (b != 0) {
             int carry = (a & b) << 1;
             a = a ^ b;
