f51150787b2a588aeea37c8fe080abccabea59fe
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-06-24 14:59:20 +0530

    Time: 1 ms (99.89%), Space: 55.9 MB (17.47%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-                int freq = 0;
-        int ans = 0;
-        for (int value : nums) {
-            if (freq == 0) {
-                ans = value;
-            }
-            if (value == ans) {
-                freq++;
-            } else {
-                freq--;
-            }
-        }
-        return ans;
+               //moore's voting algo
+               int el=nums[0];
+               int count=0;
+               for(int i=0; i<nums.length; i++){
+                if(count==0){
+                    count=1;
+                    el=nums[i];
+               }else if(nums[i]==el){
+                count++;
+               }else{
+                count--;
+               }
+               }
+            //    int count1=0;
+            //    for(int i=0; i<nums.length; i++){
+            //     if(el==nums[i])count1++;
+            //    }
+            //    if(count1>nums.length/2){
+            //     return el
+            //    }
+               return el;
     }
 }
