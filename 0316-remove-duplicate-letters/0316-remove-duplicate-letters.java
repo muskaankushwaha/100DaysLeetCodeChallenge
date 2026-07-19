346a95c9753ff5e58b5fc869c833d1474b33b0a3
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-07-19 10:35:52 +0530

    Time: 1 ms (100%), Space: 43.4 MB (86.78%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        boolean [] taken=new boolean[26];
+        int [] lidx=new int[26];
+        Arrays.fill(taken,false);
+        for(int i=0; i<s.length(); i++){
+            int idx=s.charAt(i)-'a';
+            lidx[idx]=i;
+        }
+        StringBuilder sb= new StringBuilder();
+        sb.append(s.charAt(0));
+        taken[s.charAt(0)-'a'] = true;
+        for(int i=1; i<s.length(); i++){
+            char ch=s.charAt(i);
+            int idx=ch-'a';
+            if(taken[idx])continue;
+            while(sb.length() > 0 && sb.charAt(sb.length()-1)>ch && lidx[sb.charAt(sb.length()-1)-'a'] > i){
+                char top = sb.charAt(sb.length() - 1);
+                    taken[top-'a']=false;
+                    sb.deleteCharAt(sb.length()-1);
+                    
+            }sb.append(ch);
+                taken[idx]=true;
+            
+        }
+        return sb.toString();
+    }
+}
