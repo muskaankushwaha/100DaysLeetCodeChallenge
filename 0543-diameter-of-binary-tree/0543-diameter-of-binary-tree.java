bc6183c04b58601596406d5eecd581ff62b5e310
Muskaan Kushwaha <muskaankushwaha123@gmail.com>
2026-07-22 19:55:53 +0530

    Time: 0 ms (100%), Space: 47.2 MB (27.87%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,18 +14,30 @@
  * }
  */
 class Solution {
+    // int max=-1;
+    // public int height(TreeNode root){
+    //     if(root==null)return 0;
+    //     return Math.max(height(root.left)+1,height(root.right)+1);
+    // }
+    // public int diameterOfBinaryTree(TreeNode root) {
+    //     if(root==null)return 0;
+    //     int lh=height(root.left);
+    //     int rh=height(root.right);
+    //     max=Math.max(max,lh+rh);
+    //     diameterOfBinaryTree(root.left);
+    //     diameterOfBinaryTree(root.right);
+    //     return max;
+    //now optimize in O(N);
     int max=-1;
     public int height(TreeNode root){
-        if(root==null)return 0;
-        return Math.max(height(root.left)+1,height(root.right)+1);
-    }
-    public int diameterOfBinaryTree(TreeNode root) {
         if(root==null)return 0;
         int lh=height(root.left);
         int rh=height(root.right);
         max=Math.max(max,lh+rh);
-        diameterOfBinaryTree(root.left);
-        diameterOfBinaryTree(root.right);
+        return 1+Math.max(lh,rh);
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
         return max;
     }
 }
